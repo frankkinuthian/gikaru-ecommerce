ec46f1e4ef01945a87d73ce4242b89d9903aa6f6
Frank Kinuthia <150331312+frankkinuthian@users.noreply.github.com>
2025-09-07 22:27:30 +0300

    Added product grid and live Sanity integration
    - Introduced components for displaying products and categories.
    - Added Sanity live content API integration, product/category fetching utilities
    - Updated types and Next.js image config for remote Sanity images.
    - Updated the store page to render products and categories from Sanity.

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,10 +1,23 @@
+import ProductsView from "@/components/ProductsView";
 import { Button } from "@/components/ui/button";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
+  const categories = await getAllCategories();
+
+  // console.log(
+  //   crypto.randomUUID().slice(0, 5) +
+  //     `>>> Rendered the home page cache w/ ${products.length} products and ${categories.length} categories`
+  // );
+
   return (
     <div className="">
-      <h1>Hello World!</h1>
-      <Button>Click Me!</Button>
+      {/*  Render products */}
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100">
+        <ProductsView products={products} categories={categories} />
+      </div>
     </div>
   );
 }
